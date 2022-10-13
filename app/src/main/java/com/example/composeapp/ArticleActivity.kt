a013b904e402044081cbf6ff18faa18d403d5aeb
bayazidnasir <bayazid.nasir@tokopedia.com>
2022-10-13 13:36:04 +0800

    feat : create compose article page

---
+++ app/src/main/java/com/example/composeapp/ArticleActivity.kt
@@ -0,0 +1,92 @@
+package com.example.composeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composeapp.ui.theme.BirthdayCardTheme
+
+class ArticleActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirthdayCardTheme {
+                DetailArticle()
+            }
+        }
+    }
+}
+
+@Composable
+fun TitleArticle() {
+    Text(
+        text = stringResource(R.string.title_section),
+        fontSize = 24.sp,
+        modifier = Modifier
+            .padding(16.dp)
+    )
+}
+
+@Composable
+fun FirstSectionArticle() {
+    Text(
+        text = stringResource(R.string.first_section),
+        modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+        textAlign = TextAlign.Justify
+    )
+}
+
+@Composable
+fun SecondSectionArticle() {
+    Text(
+        text = stringResource(R.string.second_section),
+        modifier = Modifier.padding(16.dp),
+        textAlign = TextAlign.Justify
+    )
+}
+
+@Composable
+fun ImageHeader(){
+    val image = painterResource(R.drawable.bg_compose_background)
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun DetailArticle() {
+    Column {
+        ImageHeader()
+        TitleArticle()
+        FirstSectionArticle()
+        SecondSectionArticle()
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun ArticlePagePreview() {
+    BirthdayCardTheme {
+        DetailArticle()
+    }
+}
