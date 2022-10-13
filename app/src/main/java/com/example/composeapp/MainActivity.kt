de8a6991e961a9ada25bcbfcc9fa041a5a6f7ec4
bayazidnasir <bayazid.nasir@tokopedia.com>
2022-10-13 12:06:11 +0800

    feat : create greeting with text

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -3,16 +3,15 @@ package com.example.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composeapp.ui.theme.BirthdayCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,10 +25,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    BirthdayGreetingWithText("happy birthday boss", "- beddu")
                 }
             }
         }
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithText(message: String, from: String) {
+    Column {
+        Text(
+            text = message,
+            fontSize = 36.sp
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp
+        )
     }
 }
 
@@ -40,6 +53,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BirthdayCardPreview() {
     BirthdayCardTheme {
-
+        BirthdayGreetingWithText("happy birthday boss", "- beddu")
     }
 }
