41f7796243e0bcb977b60af9d55fb909bfc56500
bayazidnasir <bayazid.nasir@tokopedia.com>
2022-10-14 15:08:44 +0800

    feat : create new activity and resources

---
+++ app/src/main/java/com/example/composeapp/BusinessCardActivity.kt
@@ -0,0 +1,15 @@
+package com.example.composeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class BusinessCardActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
