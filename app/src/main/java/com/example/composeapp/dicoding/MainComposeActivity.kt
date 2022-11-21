a3b0302cef426ff87d5ae12936e1faae5c2cb918
bayazidnasir <bayazid.nasir@tokopedia.com>
2022-11-21 12:44:31 +0800

    fix : add column to make it text not piled up

--- app/src/main/java/com/example/composeapp/dicoding/MainComposeActivity.kt
+++ app/src/main/java/com/example/composeapp/dicoding/MainComposeActivity.kt
@@ -3,6 +3,7 @@ package com.example.composeapp.dicoding
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -32,7 +33,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column {
+        Text(text = "Hello $name!")
+        Text(text = "Welcome to Dicoding!")
+    }
 }
 
 @Preview(showBackground = true)
