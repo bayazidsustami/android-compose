62481e30632503466d51192567337f9acbd30acc
bayazidnasir <bayazid.nasir@tokopedia.com>
2022-11-21 12:51:52 +0800

    feat : add image and set the layout

--- app/src/main/java/com/example/composeapp/dicoding/MainComposeActivity.kt
+++ app/src/main/java/com/example/composeapp/dicoding/MainComposeActivity.kt
@@ -3,16 +3,26 @@ package com.example.composeapp.dicoding
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.composeapp.R
 import com.example.composeapp.ui.theme.BirthdayCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,13 +45,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Column {
-        Text(
-            text = "Hello $name!",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
+    Row(
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(R.drawable.jetpack_compose),
+            modifier = Modifier.size(80.dp),
+            contentDescription = null
         )
-        Text(text = "Welcome to Dicoding!")
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(
+                text = "Hello $name!",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(text = "Welcome to Dicoding!")
+        }
     }
 }
 
