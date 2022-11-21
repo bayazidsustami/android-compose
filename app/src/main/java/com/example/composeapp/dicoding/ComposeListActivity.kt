00bfb08253ee46ba38f3fcc934b7f36eabbf5df0
bayazidnasir <bayazid.nasir@tokopedia.com>
2022-11-21 13:33:45 +0800

    refactor : changes text style

--- app/src/main/java/com/example/composeapp/dicoding/ComposeListActivity.kt
+++ app/src/main/java/com/example/composeapp/dicoding/ComposeListActivity.kt
@@ -34,6 +34,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -123,7 +124,12 @@ fun GreetingName(name: String) {
                     fontSize = 24.sp,
                     fontWeight = FontWeight.Bold
                 )
-                Text(text = "Welcome to Dicoding!")
+                Text(
+                    text = "Welcome to Dicoding!",
+                    style = MaterialTheme.typography.body1.copy(
+                        fontStyle = FontStyle.Italic
+                    )
+                )
             }
             IconButton(onClick = { isExpanded = !isExpanded }) {
                 Icon(
