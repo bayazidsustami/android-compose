67500da1dc13e57946439961ca289e25b51e1c1b
bayazidsustami <bayazidsustamy@gmail.com>
2022-11-26 22:15:32 +0800

    feat : complete category section

--- app/src/main/java/com/example/composeapp/dicoding/jetcoffe/JetCoffeActivity.kt
+++ app/src/main/java/com/example/composeapp/dicoding/jetcoffe/JetCoffeActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,6 +21,7 @@ import com.example.composeapp.R
 import com.example.composeapp.dicoding.jetcoffe.model.dummyCategory
 import com.example.composeapp.dicoding.jetcoffe.ui.components.CategoryItem
 import com.example.composeapp.dicoding.jetcoffe.ui.components.SearchBar
+import com.example.composeapp.dicoding.jetcoffe.ui.components.SectionText
 import com.example.composeapp.dicoding.jetcoffe.ui.theme.JetCoffeeTheme
 
 class JetCoffeActivity : ComponentActivity() {
@@ -43,6 +45,7 @@ class JetCoffeActivity : ComponentActivity() {
 fun JetCoffeeApp() {
     Column {
         Banner()
+        SectionText(stringResource(R.string.section_category))
         CategoryRow()
     }
 }
