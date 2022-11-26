3ed8880d9b3022296561ebe0b5ac4d5f3f86c794
bayazidsustami <bayazidsustamy@gmail.com>
2022-11-26 20:15:46 +0800

    feat : complete banner section

--- app/src/main/java/com/example/composeapp/dicoding/jetcoffe/JetCoffeActivity.kt
+++ app/src/main/java/com/example/composeapp/dicoding/jetcoffe/JetCoffeActivity.kt
@@ -3,13 +3,22 @@ package com.example.composeapp.dicoding.jetcoffe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composeapp.R
+import com.example.composeapp.dicoding.jetcoffe.ui.components.SearchBar
 import com.example.composeapp.dicoding.jetcoffe.ui.theme.JetCoffeeTheme
 
 class JetCoffeActivity : ComponentActivity() {
@@ -31,6 +40,24 @@ class JetCoffeActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
+    Column {
+        Banner()
+    }
+}
+
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier) {
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentScale = ContentScale.Crop,
+            contentDescription = "Banner Image",
+            modifier = Modifier.height(160.dp)
+        )
+        SearchBar()
+    }
 }
 
 @Preview(showBackground = true, device = Devices.PIXEL_4)
