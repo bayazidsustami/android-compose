85e8f779a8a4f4eb876e435609d23681a2297188
bayazidsustami <bayazidsustamy@gmail.com>
2022-11-26 23:08:07 +0800

    feat : complete coffe category page

--- app/src/main/java/com/example/composeapp/dicoding/jetcoffe/JetCoffeActivity.kt
+++ app/src/main/java/com/example/composeapp/dicoding/jetcoffe/JetCoffeActivity.kt
@@ -9,10 +9,14 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -20,10 +24,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composeapp.R
-import com.example.composeapp.dicoding.jetcoffe.model.Menu
-import com.example.composeapp.dicoding.jetcoffe.model.dummyBestSellerMenu
-import com.example.composeapp.dicoding.jetcoffe.model.dummyCategory
-import com.example.composeapp.dicoding.jetcoffe.model.dummyMenu
+import com.example.composeapp.dicoding.jetcoffe.model.*
 import com.example.composeapp.dicoding.jetcoffe.ui.components.*
 import com.example.composeapp.dicoding.jetcoffe.ui.theme.JetCoffeeTheme
 
@@ -45,23 +46,29 @@ class JetCoffeActivity : ComponentActivity() {
 }
 
 @Composable
-fun JetCoffeeApp() {
-    Column(
-        modifier = Modifier.verticalScroll(rememberScrollState())
+fun JetCoffeeApp(modifier: Modifier = Modifier) {
+    Scaffold(
+        bottomBar = { BottomBar() }
     ) {
-        Banner()
-        HomeSection(
-            title = stringResource(R.string.section_category),
-            content = { CategoryRow() }
-        )
-        HomeSection(
-            title = stringResource(R.string.section_favorite_menu),
-            content = { MenuRow(dummyMenu) }
-        )
-        HomeSection(
-            title = stringResource(R.string.section_best_seller_menu),
-            content = { MenuRow(dummyBestSellerMenu) }
-        )
+        Column(
+            modifier = modifier
+                .verticalScroll(rememberScrollState())
+                .padding(it),
+        ) {
+            Banner()
+            HomeSection(
+                title = stringResource(R.string.section_category),
+                content = { CategoryRow() }
+            )
+            HomeSection(
+                title = stringResource(R.string.section_favorite_menu),
+                content = { MenuRow(dummyMenu) }
+            )
+            HomeSection(
+                title = stringResource(R.string.section_best_seller_menu),
+                content = { MenuRow(dummyBestSellerMenu) }
+            )
+        }
     }
 }
 
@@ -107,6 +114,49 @@ fun MenuRow(
     ) {
         items(listMenu, key = {it.title}){
             MenuItem(it)
+        }
+    }
+}
+
+@Composable
+fun BottomBar(
+    modifier: Modifier = Modifier
+) {
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colors.background,
+        contentColor = MaterialTheme.colors.primary,
+        modifier = modifier
+    ) {
+        val navigationItems = listOf(
+            BottomBarItem(
+                title = stringResource(R.string.menu_home),
+                icon = Icons.Default.Home,
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_favorite),
+                icon = Icons.Default.Favorite,
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_profile),
+                icon = Icons.Default.AccountCircle,
+            )
+        )
+
+        navigationItems.map {
+            BottomNavigationItem(
+                icon = {
+                       Icon(
+                           imageVector = it.icon,
+                           contentDescription = it.title
+                       )
+                },
+                label = {
+                        Text(it.title)
+                },
+                selected = it.title == navigationItems[0].title,
+                unselectedContentColor = Color.LightGray,
+                onClick = {},
+            )
         }
     }
 }
