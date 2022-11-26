7f98ec4726ac3dae4665cf9a43db668723e62b11
bayazidsustami <bayazidsustamy@gmail.com>
2022-11-26 19:51:39 +0800

    feat : prepare jetcoffe app

---
+++ app/src/main/java/com/example/composeapp/dicoding/jetcoffe/JetCoffeActivity.kt
@@ -0,0 +1,42 @@
+package com.example.composeapp.dicoding.jetcoffe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composeapp.dicoding.jetcoffe.ui.theme.JetCoffeeTheme
+
+class JetCoffeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetCoffeeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    JetCoffeeApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun JetCoffeeApp() {
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Composable
+fun JetCoffeeAppPreview() {
+    JetCoffeeTheme {
+        JetCoffeeApp()
+    }
+}
