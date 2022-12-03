96a0e707e57b747d2eff9cf6fe8ed477cf174217
bayazidsustami <bayazidsustamy@gmail.com>
2022-12-03 12:11:27 +0800

    feat : add scroll to top button

--- app/src/main/java/com/example/composeapp/dicoding/jetheroes/JetHeroesApp.kt
+++ app/src/main/java/com/example/composeapp/dicoding/jetheroes/JetHeroesApp.kt
@@ -1,15 +1,25 @@
 package com.example.composeapp.dicoding.jetheroes
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,19 +27,44 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.composeapp.dicoding.jetheroes.model.HeroesData
 import com.example.composeapp.dicoding.jetheroes.ui.theme.JetHeroesAppTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun JetHeroesApp(
     modifier: Modifier = Modifier
 ) {
     Box(modifier) {
-        LazyColumn {
+        val scope = rememberCoroutineScope()
+        val listState = rememberLazyListState()
+        val showButton by remember {
+            derivedStateOf {listState.firstVisibleItemIndex > 0}
+        }
+        LazyColumn(
+            state = listState,
+            contentPadding = PaddingValues(bottom = 80.dp)
+        ) {
             items(HeroesData.heroes, key = {it.id}){ hero ->
                 HeroListItem(
                     name = hero.name,
                     photoUrl = hero.photoUrl
                 )
             }
+        }
+        AnimatedVisibility(
+            visible = showButton,
+            enter = fadeIn() + slideInVertically(),
+            exit = fadeOut() + slideOutVertically(),
+            modifier = Modifier
+                .padding(bottom = 30.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            ScrollToTopButton(
+                onClick = {
+                    scope.launch {
+                        listState.scrollToItem(index = 0)
+                    }
+                }
+            )
         }
     }
 }
@@ -60,6 +95,29 @@ fun HeroListItem(
                 .fillMaxWidth()
                 .weight(1f)
                 .padding(start = 16.dp)
+        )
+    }
+}
+
+@Composable
+fun ScrollToTopButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .shadow(elevation = 10.dp, shape = CircleShape)
+            .clip(CircleShape)
+            .size(56.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.White,
+            contentColor = MaterialTheme.colors.primary
+        )
+    ) {
+        Icon(
+            imageVector = Icons.Filled.KeyboardArrowUp,
+            contentDescription = null
         )
     }
 }
