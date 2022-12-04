f72e2e4c491b6f15724bbc91c87a7a0599e8fcfa
bayazidsustami <bayazidsustamy@gmail.com>
2022-12-04 15:29:39 +0800

    feat : simple ui test with compose

---
+++ app/src/androidTest/java/com/example/composeapp/dicoding/calculator/ui/CalculatorAppTest.kt
@@ -0,0 +1,49 @@
+package com.example.composeapp.dicoding.calculator.ui
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertHasNoClickAction
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import com.example.composeapp.R
+import com.example.composeapp.dicoding.calculator.ui.theme.CalculatorAppTheme
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class CalculatorAppTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Before
+    fun setup() {
+        composeTestRule.setContent {
+            CalculatorAppTheme {
+                CalculatorApp()
+            }
+        }
+    }
+
+    @Test
+    fun calculate_area_of_rectangle_correct() {
+        with(composeTestRule){
+            onNodeWithText(activity.getString(R.string.enter_length)).performTextInput("3")
+            onNodeWithText(activity.getString(R.string.enter_width)).performTextInput("4")
+            onNodeWithText(activity.getString(R.string.count)).performClick()
+            onNodeWithText(activity.getString(R.string.count), useUnmergedTree = true).assertHasNoClickAction()
+            onNodeWithText(activity.getString(R.string.result, 12.0)).assertExists()
+        }
+    }
+
+    @Test
+    fun wrong_input_not_calculated() {
+        with(composeTestRule){
+            onNodeWithText(activity.getString(R.string.enter_length)).performTextInput("..3")
+            onNodeWithText(activity.getString(R.string.enter_width)).performTextInput("4")
+            onNodeWithText(activity.getString(R.string.count)).performClick()
+            onNodeWithText(activity.getString(R.string.result, 0.0)).assertExists()
+        }
+    }
+}
