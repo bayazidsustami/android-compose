27ba07733beeccb71dd6c0ac39288fe5149b323a
bayazidsustami <bayazidsustamy@gmail.com>
2022-12-04 19:09:02 +0800

    feat : fill profile screen

--- app/src/main/java/com/example/composeapp/dicoding/jetreward/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/composeapp/dicoding/jetreward/ui/screen/profile/ProfileScreen.kt
@@ -1,11 +1,24 @@
 package com.example.composeapp.dicoding.jetreward.ui.screen.profile
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.composeapp.R
 
 @Composable
 fun ProfileScreen(
     modifier: Modifier = Modifier
 ) {
-
+    Box(
+        modifier = modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text(stringResource(R.string.profile))
+    }
 }
